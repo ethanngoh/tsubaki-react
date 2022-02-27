c1a623e8982976f4fcfb605a31be4da6d39025a7
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-02-27 01:49:25 -0600

    look i made 3 components

--- src/views/MonsterView.tsx
+++ src/views/MonsterView.tsx
@@ -1,5 +1,6 @@
 import { Button, Checkbox, FormGroup, InputGroup } from "@blueprintjs/core";
 import { handleBooleanChange } from "components/blueprintHelpers";
+import { Monster } from "protogen/monsters";
 import { GetMonsterRequest } from "protogen/monsters_api";
 import React, { Component } from "react";
 import { injectStore, withRouter } from "services";
@@ -12,6 +13,60 @@ interface MonsterViewProps {
   router: {
     params: { id: string };
   };
+}
+
+class MonsterHeader extends React.Component<any, any> {
+  constructor(props: MonsterViewProps) {
+    super(props);
+  }
+  render() {
+    const monster: Monster = this.props.monster;
+    const monsterImagePath: string = monster.monsterNoJp.toString().padStart(5, "0");
+    return (
+      <div className="monster-header">
+        <img src={`https://d30r6ivozz8w2a.cloudfront.net/media/icons/${monsterImagePath}.png`}></img>
+        <div className="monster-name">{monster.nameEn}</div>
+      </div>
+    );
+  }
+}
+
+class MonsterStats extends React.Component<any, any> {
+  constructor(props: MonsterViewProps) {
+    super(props);
+  }
+  render() {
+    const monster: Monster = this.props.monster;
+    return (
+      <div>
+        Statistics:
+        <table className="monster-stats">
+          <tr>
+            <th>HP:</th>
+            <td>{monster.hpMax}</td>
+          </tr>
+          <tr>
+            <th>ATK:</th>
+            <td>{monster.atkMax}</td>
+          </tr>
+          <tr>
+            <th>RCV:</th>
+            <td>{monster.rcvMax}</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+class MonsterSkills extends React.Component<any, any> {
+  constructor(props: MonsterViewProps) {
+    super(props);
+  }
+  render() {
+    const monster: Monster = this.props.monster;
+    return <div className="monster-skills"></div>;
+  }
 }
 
 export default class MonsterView extends Component<MonsterViewProps, State> {
@@ -38,17 +93,12 @@ export default class MonsterView extends Component<MonsterViewProps, State> {
     if (!monster) {
       return <div>Loading...</div>;
     }
-    const name_en = monster.cost;
-    console.log(monster);
     return (
       <>
-        <h1>Kittens</h1>
-
-        <div>
-          DEBUG:
-          <div>{JSON.stringify(monster)}</div>
-          kittens
-          <div>{name_en}</div>
+        <div className="main-container">
+          <MonsterHeader monster={monster} />
+          <MonsterStats monster={monster} />
+          <MonsterSkills monster={monster} />
         </div>
       </>
     );
