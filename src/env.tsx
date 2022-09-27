b579d42f375c2ea2d4083d48d573b3452cd687b2
ethanngoh <ethan@ethangoh.com>
2022-09-27 14:11:21 -0700

    env + gh pages

--- src/env.tsx
+++ src/env.tsx
@@ -1,3 +1,9 @@
 import { OpenAPI } from "./client";
 
-OpenAPI.BASE = "http://localhost:8001";
+const environment = process.env.REACT_APP_ENVIRONMENT ? process.env.REACT_APP_ENVIRONMENT : "development";
+
+if (environment == "development") {
+  OpenAPI.BASE = "http://localhost:8001";
+} else if (environment == "production") {
+  OpenAPI.BASE = "http://api.tsubakibot.com:8001";
+}
